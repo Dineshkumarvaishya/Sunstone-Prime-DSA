612fd00d0ae51c4751c27ab39077afbbd7e77ad7
Dineshkumarvaishya <dineshvaishya210@gmail.com>
2026-02-04 15:20:59 +0530

    done

--- Lecture/lecture 14/reverse.cpp
+++ Lecture/lecture 14/reverse.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main()
 {
-    int
+    int ans, var=0,n;
+    cin>>n;
+    while(n>0)
+    {
+        ans=n%10;
+        var=(var*10)+ans;
+        n=n/10;
+        
+    }
+    cout<<var;
 }
